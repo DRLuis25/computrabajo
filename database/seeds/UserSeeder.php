7e899756f4e0720dea6bf298e2fb2f06738d0dbf
Jandro877 <79387649+Jandro877@users.noreply.github.com>
2021-08-03 21:56:30 -0500

    database

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = "Bryan Alejandro";
+        $user->apellidos = "Romero Osorio";
+        $user->email = "bromero@unitru.edu.pe";
+        $user->password = "$2y$10$7/xG2DyptIBjd5pW7OfTKegmryR.m7wqoleQQlSv1rwFCj1H0Mxlm"; // 123456789
+        $user->save();
+
+        $user = new User();
+        $user->name = "Maximo Alexander";
+        $user->apellidos = "Fernández Munóz";
+        $user->email = "mfernandez@unitru.edu.pe";
+        $user->password = "$2y$10$7/xG2DyptIBjd5pW7OfTKegmryR.m7wqoleQQlSv1rwFCj1H0Mxlm"; // 123456789
+        $user->save();
+    }
+}
