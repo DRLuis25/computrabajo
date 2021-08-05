16dbe181c05414caab37c8645f14e5a1480576ce
Luis Guillermo <ldelgado@unitru.edu.pe>
2021-08-04 22:54:57 -0500

    Cambios migraciones. Agregado relaciones entre las tablas departamento, ciudad y distrito.

--- database/factories/CiudadFactory.php
+++ database/factories/CiudadFactory.php
@@ -3,11 +3,15 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\Ciudad;
+use App\Models\Departamento;
 use Faker\Generator as Faker;
 
 $factory->define(Ciudad::class, function (Faker $faker) {
 
     return [
+        'departamento_id' => function() {
+            return factory(Departamento::class)->create()->id;
+        },
         'nombre' => $faker->city,
         'created_at' => $faker->date('Y-m-d H:i:s'),
         'updated_at' => $faker->date('Y-m-d H:i:s'),
