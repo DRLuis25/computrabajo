ab8fb906a1164029efa83b8cc93520f9d64bd382
MaxMayo23 <77753027+MAXIMO45@users.noreply.github.com>
2021-09-02 03:07:18 -0500

    Cambio02

--- resources/views/layouts/admin/rdiarios.blade.php
+++ resources/views/layouts/admin/rdiarios.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <h5 style="margin-left: 45%">REPORTES DIARIOS</h5>
-<div class="card">
+<div class="card" style="margin:15px ">
     <div class="card-body">
 
         <div  class="row">
@@ -14,13 +14,50 @@
                         <select name="mes" id="mes" class="form-control col-3" required>
                             <option value="">Seleccione un mes</option>
                             <option value="01">Enero</option>
+                            <option value="02">Febrero</option>
+                            <option value="03">Marzo</option>
+                            <option value="04">Abril</option>
+                            <option value="05">Mayo</option>
+                            <option value="06">Junio</option>
+                            <option value="07">Julio</option>
                             <option value="08">Agosto</option>
+                            <option value="09">Septiembre</option>
                         </select>
                         <select name="dia" id="dia" class="form-control col-3" required>
                             <option value="">Seleccione un dia</option>
+                            <option value="01">01</option>
+                            <option value="02">02</option>
+                            <option value="03">03</option>
+                            <option value="04">04</option>
+                            <option value="05">05</option>
+                            <option value="06">06</option>
+                            <option value="07">07</option>
+                            <option value="08">08</option>
+                            <option value="09">09</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                            <option value="13">13</option>
+                            <option value="14">14</option>
+                            <option value="15">15</option>
+                            <option value="16">16</option>
+                            <option value="17">17</option>
+                            <option value="18">18</option>
+                            <option value="19">19</option>
+                            <option value="20">20</option>
+                            <option value="21">21</option>
+                            <option value="22">22</option>
+                            <option value="23">23</option>
+                            <option value="24">24</option>
                             <option value="25">25</option>
-                            <option value="26" selected>26</option>
+                            <option value="26">26</option>
+                            <option value="27">27</option>
+                            <option value="28">28</option>
+                            <option value="29">29</option>
+                            <option value="30">30</option>
+                            <option value="31">31</option>
                         </select>
+
                         <input type="submit" value="Consultar" class="btn btn-primary col-2">
                     </div>
                 </form>
