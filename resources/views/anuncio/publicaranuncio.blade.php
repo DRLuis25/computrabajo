3bde704690a3e619c6cbd11f3e9e7b38ee75cd52
Luis Guillermo <ldelgado@unitru.edu.pe>
2021-09-01 23:40:17 -0500

    cambio header publicar anuncio

--- resources/views/anuncio/publicaranuncio.blade.php
+++ resources/views/anuncio/publicaranuncio.blade.php
@@ -1,24 +1,6 @@
 @extends('layouts.app')
 @section('content')
-
-<header class="p-3">
-    <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
-                    <use xlink:href="#bootstrap"></use>
-                </svg>
-            </a>
-            <div class="text-end">
-                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li><a href="{{ route("anuncio.misanuncios") }}" class="nav-link px-2 text-secondary">Mis anuncios</a></li>
-                    <li><a href="{{ route("anuncio.publicaranuncio") }}" class="nav-link px-2 text-secondary">Publicar un Anuncio</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</header>
-
+@include('anuncio.header')
 <form method="POST" action="{{route('anuncio.guardaranuncio')}}">
 @csrf
 <div style="margin-left:12%; width: 76%;">
